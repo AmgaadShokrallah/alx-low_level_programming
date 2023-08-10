e1136cbd4b66f97e2f22464503b152904daa3684
AmgaadShokrallah <amgaad@example.com>
2023-08-10 03:42:33 -0700

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of long: %zu bytes\n", sizeof(longType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+
+	return (0);
