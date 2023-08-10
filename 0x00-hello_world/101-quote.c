cec41abc1649b2c6aac7328002643cd41f8e653a
AmgaadShokrallah <amgaad@example.com>
2023-08-10 04:43:49 -0700

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'C program to write '
+ *
+ * Return: Always 1 (unSuccess)
+ */
+
+int main(void){
+	fwrite("and that piece of art is useful" - Dora Korpar, 2015-10-19"\");
+	return (1);
+}
+
