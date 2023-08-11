968da91aff9daa7275aa6c0417ab224388839cde
AmgaadShokrallah <amgaad@example.com>
2023-08-11 02:53:04 -0700

    C - Variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
-		printf("%i and is greater than 5\n" n);
+		printf("%i and is greater than 5\n", n);
 	else if (n == 0)
-		printf("%i and is 0\n" n);
+		printf("%i and is 0\n", n);
 	else
-		printf("and is less than 6 and not 0\n" n);
+		printf("and is less than 6 and not 0\n", n);
 	return (0);
 }
