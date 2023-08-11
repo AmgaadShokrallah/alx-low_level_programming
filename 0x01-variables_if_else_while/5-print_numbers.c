118f97baedb5ab92463e04332d1e66e4dd7dd4e8
AmgaadShokrallah <amgaad@example.com>
2023-08-11 06:15:18 -0700

    C - Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program c'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i\n", i)
+	}
+	return (0);
+}
