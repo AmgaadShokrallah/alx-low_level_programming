03d05d40f9f4ee186bdfa795cc00116e3b2b5c1f
AmgaadShokrallah <amgaad@example.com>
2023-08-11 05:37:54 -0700

    C - Variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program c'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+			c++;
+	}
+	putchar('\n');
+	return (0);
+}
