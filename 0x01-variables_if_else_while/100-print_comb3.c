02b43c37611d605045d85818b1181c8ece0c437a
AmgaadShokrallah <amgaad@example.com>
2023-08-14 01:39:54 -0700

    C - Variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program c'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i1 = 0;
+	int i2;
+
+	while (i1 <= 9)
+	{
+		i2 = 0;
+		while (i2 <= 9)
+		{
+			if (i1 != i2 && i1 < i2)
+			{
+				putchar(i1 + 48);
+				putchar(i2 + 48);
+
+				if (i1 + i2 != 17)
+				{
+					putchar(",");
+					putchar(" ");
+				}
+
+
+			}
+			i2++;
+
+		}
+		i1++;
+	}
+	putchar("\n");
+	return (0);
+}
