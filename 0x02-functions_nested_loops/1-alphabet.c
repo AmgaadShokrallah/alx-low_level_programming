2165b311f15129ce19e1398050727c57e5e30c54
AmgaadShokrallah <amgaad@example.com>
2023-08-15 03:45:25 -0700

    C - Functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: ' prints the alphabet, in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+
+}
