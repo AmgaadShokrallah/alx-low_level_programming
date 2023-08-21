d1cc6574223816bb577ab113c315f50ce572ce65
AmgaadShokrallah <amgaad@example.com>
2023-08-21 13:04:07 -0700

    C - Pointers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for 101-crackme.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int sum;
+	char ch;
+
+	srand(time(NULL));
+
+}
