e1c548f9bc6e9af97d883aade9faec7154570e71
AmgaadShokrallah <amgaad@example.com>
2023-08-23 08:27:28 -0700

    C - More pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,6 @@
 
 /**
  * reverse_array - everses the content of an array of integers.
- *
  * @a: an array of integers
  * @n: the number of elements to swap
  *
@@ -14,7 +13,7 @@ void reverse_array(int *a, int n)
 {
 	int i, j, tmp;
 
-	for (i = 0; j = (n - 1); i < j; i++, j--)
+	for (i = 0, j = (n - 1); i < j; i++, j--)
 	{
 		tmp = a[i];
 		a[i] = a[j];
