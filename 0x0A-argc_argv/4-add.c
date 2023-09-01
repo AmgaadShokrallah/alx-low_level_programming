a5ce1339694dafca57e18f55130e357fc3cddc29
AmgaadShokrallah <amgaad@example.com>
2023-09-01 11:32:10 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: parameter1
+ * @argv: parameter2
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	int result = 0;
+	char *ch;
+
+	while (--argc)
+	{
+		for (ch = argv[argc]; *ch; ch++)
+			if (*ch < 48 || *ch > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		result += atoi(argv[argc]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
