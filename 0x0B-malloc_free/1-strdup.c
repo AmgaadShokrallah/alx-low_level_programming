6760a6415a5ed30b89ee02d079cf6453c3467a21
AmgaadShokrallah <amgaad@example.com>
2023-09-04 16:39:56 -0700

    C - malloc, free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,7 +20,7 @@ char *_strdup(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	s = (char *)malloc(l * sizeof(char) + 1);
+	s = malloc(sizeof(char) * (l + 1))
 	if (s == NULL)
 		return (NULL);
 
