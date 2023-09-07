5a41cac7c80652fda0185b93a7c8a97b89bc7272
AmgaadShokrallah <amgaad@example.com>
2023-09-07 16:47:24 -0700

    More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,7 +38,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	c = ptr;
 
-	for (i = 0; i < old_size && i < new_size; i++)
+	for (i = 0; i < old_size; i++)
 		s[i] = c[i];
 	free(ptr);
 	return (s);
