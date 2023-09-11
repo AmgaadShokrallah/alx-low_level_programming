71f2ee4d4de67d876c3bb9f58fec2ba1be78bd8d
AmgaadShokrallah <amgaad@example.com>
2023-09-10 21:11:04 -0700

    Structures, typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog.
+ * @d: parameter1
+ * @name: parameter2
+ * @age: parameter3
+ * @owner: parameter4
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,6 @@ struct dog
 	char *owner;
 } dogy;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
