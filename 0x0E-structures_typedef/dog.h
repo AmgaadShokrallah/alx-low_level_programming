997aca36daba91161f9110c199b453568b75d36d
AmgaadShokrallah <amgaad@example.com>
2023-09-10 20:55:58 -0700

    Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for a dog
+ * @name: parameter1
+ * @age: parameter2
+ * @owner: parameter3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dogy;
+
+#endif
