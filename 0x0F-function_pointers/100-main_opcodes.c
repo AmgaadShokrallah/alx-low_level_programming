f8f4fe01ba38650c453141540f41892d872ad072
AmgaadShokrallah <amgaad@example.com>
2023-09-13 00:46:02 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include "Function pointers"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: parameter1
+ * @argv: parameter2
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int by, i;
+	char *array = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	by = atoi(argv[1]);
+
+	if (by < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < by; i++)
+	{
+		if (i == by - 1)
+		{
+			printf("%02hhx\n", array[i]);
+			break;
+		}
+		printf("%02hhx ", array[i]);
+	}
+	return (0);
+}
