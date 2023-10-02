8df3b8befb3cd35703977d0afb032b0e200b5d01
AmgaadShokrallah <amgaad@example.com>
2023-10-02 06:26:29 -0700

    File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - that appends text at end of file.
+ * @filename: parameter1
+ * @text_content: parameter2
+ *
+ * Return: return 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int lenght = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != '\0')
+	{
+		for (lenght = 0; text_content[lenght];)
+			lenght++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, lenght);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
