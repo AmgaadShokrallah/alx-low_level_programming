d48a960427edfa7fc4b38cb4338ec1880fab4e3b
AmgaadShokrallah <amgaad@example.com>
2023-10-02 13:30:30 -0700

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,31 +13,32 @@
 * Return: 1 on success 0 on failure
 */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int fd_from = 0, fd_to = 0;
-	ssize_t f;
-	char buff[READ_BUF_SIZE];
+	ssize_t byte;
+	char buf[BUF_SIZE];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
 
-	while ((f == read(fd_from, buff, READ_BUF_SIZE)) > 0)
-		if (write(fd_to, buff, f) != f)
+	while ((byte = read(fd_from, buf, BUF_SIZE)) > 0)
+		if (write(fd_to, buf, byte) != byte)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
-	if (f == -1)
+	if (byte == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
-	if (close(fd_from) == -1)
+	fd_from = close(fd_from);
+	fd_to = close(fd_to);
+	if (fd_from)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
-	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_to), exit(100);
+	if (fd_to)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
 
 	return (0);
 }
