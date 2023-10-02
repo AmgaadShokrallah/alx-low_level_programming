819e1e431d02099c3de0f1ac7b58c14274c00b42
AmgaadShokrallah <amgaad@example.com>
2023-10-02 12:51:47 -0700

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,27 +17,27 @@ int main(int argc, char *argv[])
 {
 	int fd_from = 0, fd_to = 0;
 	ssize_t f;
-	char buff[1024];
+	char buff[READ_BUF_SIZE];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
-	while (f == 1024)
-	{
-		f = read(fd_from, buff, 1024);
-		if (f == -1)
-			dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+
+	while ((f == read(fd_from, buff, READ_BUF_SIZE)) > 0)
 		if (write(fd_to, buff, f) != f)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
-	}
+	if (f == -1)
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
 
 	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
+
 	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_to), exit(100);
 
